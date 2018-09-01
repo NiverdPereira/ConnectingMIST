df1f484d5c14348a1647d44ea3c871edd2964656
Pereira <niverdpereira@gmail.com>
2018-09-01 23:22:18 +0600

    Added Home and transition between login and home

--- app/src/main/java/e/par/connectingmist/LoginActivity.java
+++ app/src/main/java/e/par/connectingmist/LoginActivity.java
@@ -1,13 +1,26 @@
 package e.par.connectingmist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    CardView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login= (CardView)findViewById(R.id.cardView);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(LoginActivity.this,Home.class);
+                startActivity(i);
+            }
+        });
     }
 }
